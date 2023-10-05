bc5addeb725b437ae9d0c513998d7c82d0b65a76
donghun <iandonghun@gmail.com>
2023-10-05 21:22:15 +0900

    Add version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 // App.js
 import React from "react";
 import GameBoard from "./components/GameBoard";
+import PackageJson from "../package.json";
 
 function App() {
   return (
     <div className="App">
-      <h1>수박 게임 베타</h1>
+      <h1>
+        수박 게임 베타{" "}
+        <span
+          style={{
+            color: "grey",
+            fontSize: "0.5em",
+          }}
+        >
+          v{PackageJson.version}
+        </span>
+      </h1>
       <GameBoard />
     </div>
   );
